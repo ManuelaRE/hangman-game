cc5383d7ff1bfbeb149ab5e8b462cf36f155526a
Manuela Redinciuc <manuela.redinciuc@Nology.local>
2020-10-02 09:05:54 +0100

    deleted un-used documents

--- dist/words.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var wordsToGuess = ["javascript", "iterator", "variables", "constant", "function", "application", "crabs", "sharks", "wolffish", "mariana", "asynchronous", "classes", "object", "looping", "callback", "implement", "responsive"];
-var _default = wordsToGuess;
-exports["default"] = _default;
